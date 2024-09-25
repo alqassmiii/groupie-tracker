98b7da298684a2abc6ca70411b813adce402315d
alqassmiii <qassimhassan9@gmail.com>
2024-09-25 20:40:25 +0300

    90%

---
+++ API/handel.go
@@ -0,0 +1,61 @@
+package API
+
+import (
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+func MainPage(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimSpace(r.URL.Path)
+	if path != "/" {
+		http.Error(w, "404 error", http.StatusNotFound)
+		return
+	}
+	data := ArtistData()
+	tmpl, err := template.ParseFiles("templates.html")
+	if err != nil {
+		http.Error(w, "500 swefderver error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "500 eeeserver error", http.StatusInternalServerError)
+	}
+}
+func ArtistPage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/artistInfo" {
+		http.Error(w, "404 error", http.StatusNotFound)
+		return
+	}
+	artistName := r.URL.Query().Get("name")
+	if artistName == "" {
+		http.Error(w, "400 Bad Request", http.StatusBadRequest)
+		return
+	}
+	data := CollectData()
+	artistExists := false
+	var selectedArtist Data
+	for _, artist := range data {
+		if artist.A.Name == artistName { //artistName is the one from the url
+			selectedArtist = artist
+			artistExists = true
+			break
+		}
+	}
+	if !artistExists {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	tmpl, err := template.ParseFiles("artistPage.html")
+	if err != nil {
+		http.Error(w, "500 seeerver error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	err = tmpl.Execute(w, selectedArtist)
+	if err != nil {
+		http.Error(w, "500 ffeserver error", http.StatusInternalServerError)
+	}
+}
--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"groupie-tracker/API"
-	"html/template"
+	API "groupie-tracker/API"
 	"log"
 	"net/http"
 )
@@ -14,32 +13,13 @@ func init() {
 	AA = API.CollectData()
 }
 
-func HomePage(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("artestPage.html") // Updated path
-	if err != nil {
-		http.Error(w, "Error parsing template", http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, AA) // Updated variable
-	if err != nil {
-		http.Error(w, "Error executing template", http.StatusInternalServerError)
-		return
-	}
-
-}
-
-func ArtestPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the artestPage!")
-}
-
 func main() {
 	printAA()
-	http.HandleFunc("/", HomePage)
-	http.HandleFunc("/artestPage", ArtestPage)
-
-	fmt.Println("Server is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", API.MainPage)
+	mux.HandleFunc("/artistInfo", API.ArtistPage)
+	fmt.Println("Server is listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
 func printAA() {
